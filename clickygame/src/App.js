a224b70a0afdce95860a46fc2b815d9045dca5e5
anthonyknight023@gmail.com <anthonyknight023@gmail.com>
2018-04-26 21:05:55 -0400

    Got a functional app

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -9,33 +9,54 @@ class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     cards: cards,
-    score: 0,
-    topScore: 0,
+    score: 0,   
     clickedCards: [],
     footerText: ""
   }
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+  reArrangeCards = (array) => {
+    let currentIndex = array.length;
+
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      let temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }    
+  }
+
+  checkCard = (id) => {
+    
+    if(this.state.clickedCards.includes(id)) {
+      console.log("LOST");
+      this.setState({score: 0});
+    }
+    else {
+      this.setState({score: this.state.score + 1});
+      this.state.clickedCards.push(id);
+      this.reArrangeCards(this.state.cards);
+
+    }
+   
+  }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title score={this.state.score} topScore={this.state.topScore}>ClickY Game</Title>
-        {this.state.cards.map(friend => (
+        <Title score={this.state.score}>Clicky Game</Title>
+        {this.state.cards.map(card => (
           <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+          checkCard={this.checkCard}
+            id={card.id}
+            key={card.id}
+            name={card.name}
+            image={card.image}
+            // occupation={friend.occupation}
+            // location={friend.location}
           />
         ))}
       </Wrapper>
